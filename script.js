2016fe8b0cd2287b7c7f7c11e20fdfdba2568dd2
Si0dot7 <felm27010@gmail.com>
2025-04-03 18:16:54 +0700

    fix css overview and padding every pages

--- script.js
+++ script.js
@@ -3,11 +3,11 @@ function loadPage(path) {
         if (status == "error") {
             $('#other-content').html(`<p style="color:red;">ไม่สามารถโหลดหน้า ${path} ได้</p>`);
         } else {
-            // เมื่อโหลดหน้าเสร็จ เช็คว่าหน้าเป็น OverView.html ไหม
-            if (path === "/pages/OverView.html") {
+            
+            if (path === "./pages/OverView.html") {
                 setTimeout(() => {
                     initializeCharts("budgetChart");
-                }, 100); // ดีเลย์เล็กน้อยเพื่อให้ Element ถูกสร้างก่อน
+                }, 100); 
             }
           }
     });
